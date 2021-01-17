3e8ca55fd3fc240c270457aab76a12b7c4e4a4eb
Haneul Jeong <haneulj0510@gmail.com>
2021-01-17 18:50:27 +0900

    1/7 실습2 복습

---
+++ day3review_2.py
@@ -0,0 +1,138 @@
+# 1/7 실습2
+
+# 1번
+print("<<1번>>")
+#    함수명 : print_book_title
+#    매개변수 : 없음, 리턴값 : 없음, 기능 : 파이썬 교재명을 화면에 출력
+#    함수명 : print_book_publisher
+#    매개변수 : 없음, 리턴값 : 없음, 기능 : 파이썬 교재의 출판사명을 화면에 출력
+def print_book_title():
+    print("파이썬 정복")
+def print_book_publisher():
+    print("한빛미디어")
+print_book_title()
+print_book_publisher()
+
+print("\n")
+# 2번
+print("<<2번>>")
+# 함수명 : get_book_title
+#    매개변수 : 없음, 리턴값 : 있음, 기능 : 파이썬 교재명을 리턴한다.
+#    함수명 : get_book_publisher
+#    매개변수 : 없음, 리턴값 : 있음, 기능 : 파이썬 교재의 출판사명을 리턴한다
+# get_book_title() 함수를 호출하고 리턴되는 결과를 name 변수에 저장한 다음 name 변수의 값을 2회 출력한다.
+# get_book_publisher() 함수를  호출하고 리턴되는 결과를 화면에 출력한다.
+def get_book_title():
+    return "파이썬 정복"
+def get_book_publisher():
+    return "한빛미디어"
+
+name = get_book_title()
+print(name * 2)
+print(get_book_publisher())
+
+
+print("\n")
+# 3번
+print("<<3번>>")
+# 함수명 : expr
+# 매개변수 : 숫자 2개와 연산자 1개, 리턴값 : 연산 결과 또는  None
+# 기능 : 전달된 두 개의 숫자에 대해서 전달된 연산을 처리하고 그 결과를 리턴한다.
+# 연산자는 +, -, *, / 만 처리하며 그 외의 연산자는 연산을 하지 않고 None 을 리턴한다.
+# 숫자 2개와 연산자 1개를 전달하여 expr() 이라는 함수를 호출한 다음 리턴 결과가 None 이면 수행 불가를 출력하고 그렇지 않으면 연산결과 : XX 를 출력한다.
+def expr(n1,n2,c):
+    if c == "+" or c == "-" or c == "*" or c == "/":
+        if c == "+":
+            cal= n1+n2
+        elif c == "-":
+            cal = abs(n1-n2)
+        elif c == "*":
+            cal = n1*n2
+        elif c == "/":
+            cal = n1/n2
+        print("연산 결과:", cal)
+    else:
+        cal = None
+        print("수행 불가")
+    return cal
+
+expr(1,6,"*")
+expr(2,6,"$")
+
+
+print("\n")
+
+# 4번
+print("<<4번>>")
+# 함수명 : print_triangle
+# 매개변수 : 1개, 리턴값 : 없음, 기능 : 전달된 숫자 개수로 구성되는 삼각형을 출력
+# 전달되는 아규먼트 값은 1~10으로 제한한다. 1~10 이외의 값이 전달된 경우에는 처리하지 않고 그냥 리턴한다.
+
+def print_triangle(n):
+    if 0 < n <= 10:
+        for i in range(1,n+1):
+            print("*" * i)
+    else:
+        return
+
+print_triangle(4)
+print_triangle(11)
+
+print("\n")
+
+# 5번
+print("<<5번>>")
+# 함수명 : print_uptriangle
+# 매개변수 : 1개, 리턴값 : 없음, 기능 : 전달된 숫자 개수로 구성되는 역삼각형을 출력
+def print_uptriangle(n):
+    if 0 < n <= 10:
+        for i in range(n,0,-1):
+            print("@" * i)
+    else:
+        return
+
+print_uptriangle(6)
+print_uptriangle(15)
+
+print("\n")
+
+# 6번
+print("<<6번>>")
+# 함수명 : print_gugudan
+# 매개변수 : 1개, 리턴값 : 없음, 기능 : 전달된 숫자의 구구단을 출력한다.
+# 전달된 아규먼트가 int 타입인지 채크하고 int 타입이 아니면 그냥 리턴한다.
+# 전달된 아규먼트가 1~9 사이인지 채크하고 아니면 그냥 리턴한다.
+# 그 외의 경우에는 해당 단의 구구단을 행 단위로 출력한다.
+# 숫자를 다양하게 지정해서 print_ gugudan() 함수를 호출해 본다.
+def print_gugudan(n):
+    if type(n) == int:
+        if 1 <= n <= 9:
+            for i in range(1,10):
+                print(n,"*",i,"=",n*i)
+
+print_gugudan(6)
+
+print("\n")
+
+# 7번
+print("<<7번>>")
+# 함수명 : differtwovalue
+# 매개변수 :  2개, 리턴값 : 연산 결과
+# 기능 : 전달받은 2개의 데이터 중에서 큰 값에서 작은 값의 차를 리턴 두 값이 동일하면 0 을 리턴한다.
+#       10, 20 이 전달되면 ---> 10 리턴
+#       20, 5 가 전달되면 ---> 15 리턴
+#       5, 30 이 전달되면 ---> 25 리턴
+#       6, 3 이 전달되면  ---> 3 리턴
+# 1부터 30 사이의 난수 2 개를 추출하여 2번에서 구현된 함수를 호출하고 결과를 다음 형식으로 출력한다.
+# "X 와 Y 의 차는 W 입니다."
+
+def differtwovalue(a,b):
+    r= abs(a-b)
+    return r
+
+import random
+for i in range(1,5):
+    a = random.randint(1,30)
+    b = random.randint(1,30)
+    print(a,"와",b,"의 차는", differtwovalue(a,b),"입니다.")
+
